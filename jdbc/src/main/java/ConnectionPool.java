b57449665e1539a5aaee17c652069c730e0b0f52
Lena <ellenageor@gmail.com>
2016-10-25 19:39:51 +0300

    jdbc edit

---
+++ jdbc/src/main/java/ConnectionPool.java
@@ -0,0 +1,63 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+class ConnectionPool implements AutoCloseable {
+    private BlockingQueue<PooledConnection> connectionQueue;
+    private static ConnectionPool instance;
+
+    public static ConnectionPool getInstance(String pathConfig) {
+        if (instance == null) {
+            synchronized (ConnectionPool.class) {
+                if (instance == null) {
+                    instance = new ConnectionPool(pathConfig);
+                }
+            }
+        }
+        return instance;
+    }
+
+    private ConnectionPool(String pathToConfig) {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(pathToConfig));
+            Class.forName(properties.getProperty("driver"));
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
+        String url = properties.getProperty("url");
+        int poolSize = Integer.parseInt(properties.getProperty("poolSize"));
+
+        connectionQueue = new ArrayBlockingQueue<>(poolSize);
+
+        for (int i = 0; i < poolSize; i++) {
+            try {
+                connectionQueue.add(new PooledConnection(DriverManager.getConnection(url, properties), this));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    Connection getConnection() throws InterruptedException {
+        return connectionQueue.take();
+    }
+
+    void acceptConnection(PooledConnection connection) {
+        connectionQueue.offer(connection);
+    }
+
+    @Override
+    public void close() throws Exception {
+        for (PooledConnection connection : connectionQueue) {
+            connection.reallyClose();
+        }
+    }
+}
+
---
+++ jdbc/src/main/java/User.java
@@ -0,0 +1,44 @@
+public class User {
+    private final int id;
+    private final String name;
+    private final String lastName;
+    private final String email;
+    private final String password;
+    private final String userName;
+
+    public User(int id, String name, String lastName, String email, String password, String userName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        return userName != null ? userName.equals(user.userName) : user.userName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        return result;
+    }
+}
