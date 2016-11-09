936b88baea3f69cc563ed0caddc825076de0c929
Lena <ellenageor@gmail.com>
2016-11-09 16:56:27 +0300

    login filter added

--- servlets/src/main/java/security/LoginFilter.java
+++ servlets/src/main/java/security/LoginFilter.java
@@ -10,26 +10,23 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/page/userPage.html", "/page/uploadPhoto.html"})
-public class SecurityFilter extends HttpFilter {
+@WebFilter(urlPatterns = {"/getin/login.html"})
+public class LoginFilter extends HttpFilter {
     private static final String KEY = "key";
-
     @Override
-    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpSession session = request.getSession();
 
-        if (session.getAttribute(KEY) != null) {
-            System.out.println("key not null filter");
-            chain.doFilter(request, response);
-        } else {
-            System.out.println("key is null in filter check");
-            forward("/getin/login.html", request,response);
+        if (session.getAttribute(KEY)!= null){
+            System.out.println("key not null in login");
+            forward("/getin/alreadyLoged.html", request,response);
+        }
+        else{
+            System.out.println("key null in login");
+            chain.doFilter(request,response);
         }
     }
-
     private void forward(String path, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher(path).forward(request, response);
     }
-
 }
-
